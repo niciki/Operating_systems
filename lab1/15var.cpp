34a4fe5ec7632a4ce88acf19189d0302397c8ae3
niciki <71276784+niciki@users.noreply.github.com>
2021-09-19 01:52:39 +0300

    Update 15var.cpp

--- lab1/15var.cpp
+++ lab1/15var.cpp
@@ -7,17 +7,17 @@ int main(){
     string name_of_file;
     string s;
     if(pipe(fd1) == -1){
-        cout << "Error during creating pipe1\n";
+        std::cout << "Error during creating pipe1\n";
         return 1;
     }
     if(pipe(fd2) == -1){
-        cout << "Error during creating pipe2\n";
+        std::cout << "Error during creating pipe2\n";
         return 4;
     }
     int id = fork();
     switch(id){
         case -1:
-            cout << "Error during creating fork\n";
+            std::cout << "Error during creating fork\n";
             return 2;
         case 0:
             printf("[%d] It's child\n", getpid());
@@ -25,15 +25,15 @@ int main(){
             close(fd2[0]);
             read(fd1[0], &s, sizeof(string));
             if(65 <= int(s[0]) and int(s[0]) <= 90){
-                f.printf(s);
+                fprintf(f, "%s", s);
             } else {
                 write(fd2[1], &s, sizeof(string))
             }
             close(fd1[0]);
             close(fd2[1]);
         default:
-            cout << "Print input name of file: ";
-            cin >> name_of_file;
+            std::cout << "Print input name of file: ";
+            std::cin >> name_of_file;
             FILE *f;
             if ((f = fopen(name_of_file, "w"))==NULL) {
                 printf("Cannot open file.\n");
@@ -42,13 +42,13 @@ int main(){
             printf("[%d] It's parent. Child id: %d\n", getpid(), id);
             close(fd1[0]);
             close(fd2[1]);
-            cin >> s;
+            std::cin >> s;
             while(s){
                 write(fd1[1], &s, sizeof(string));
-                cin >> s;
+                std::cin >> s;
             }
             read(fd2[0], &s, sizeof(string));
-            cout << "Error in string " << s << "\n";
+            std::cout << "Error in string " << s << "\n";
 		    close(fd1[1]);
     }   
 }
