0baf6af9d9d7585e17cc9fb825cb49c0c2b4c339
niciki <71276784+niciki@users.noreply.github.com>
2021-09-19 01:36:26 +0300

    Update 15var.cpp

--- lab1/15var.cpp
+++ lab1/15var.cpp
@@ -2,14 +2,17 @@
 #include "stdio.h"
 
 int main(){
-    int fd[2];
+    int fd1[2];
+    int fd2[2];
     string name_of_file;
     string s;
-    cout << "Print input name of file: ";
-    cin >> name_of_file;
-    if(pipe(fd) == -1){
-        cout << "Error during creating pipe\n";
+    if(pipe(fd1) == -1){
+        cout << "Error during creating pipe1\n";
         return 1;
+    }
+    if(pipe(fd2) == -1){
+        cout << "Error during creating pipe2\n";
+        return 4;
     }
     int id = fork();
     switch(id){
@@ -18,7 +21,34 @@ int main(){
             return 2;
         case 0:
             printf("[%d] It's child\n", getpid());
+            close(fd1[1]);
+            close(fd2[0]);
+            read(fd1[0], &s, sizeof(string));
+            if(65 <= int(s[0]) and int(s[0]) <= 90){
+                f.printf(s);
+            } else {
+                write(fd2[1], &s, sizeof(string))
+            }
+            close(fd1[0]);
+            close(fd2[1]);
         default:
+            cout << "Print input name of file: ";
+            cin >> name_of_file;
+            FILE *f;
+            if ((f = fopen(name_of_file, "w"))==NULL) {
+                printf("Cannot open file.\n");
+                return 3;
+            }
             printf("[%d] It's parent. Child id: %d\n", getpid(), id);
+            close(fd1[0]);
+            close(fd2[1]);
+            cin >> s;
+            while(s){
+                write(fd1[1], &s, sizeof(string));
+                cin >> s;
+            }
+            read(fd2[0], &s, sizeof(string));
+            cout << "Error in string " << s << "\n";
+		    close(fd1[1]);
     }   
 }
